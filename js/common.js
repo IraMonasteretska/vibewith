f784e2cc6feb3946e96eeccf792edafad5f1e85c
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2022-12-10 09:24:32 +0200

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -9,3 +9,18 @@ burgerBtn.addEventListener('click', function () {
 closeMenu.addEventListener('click', function () {
     menu.classList.remove('show');
 });
+
+AOS.init();
+
+$('.joinlink, .scrolllink').on( 'click', function(){ 
+    var el = $(this);
+    var dest = el.attr('href'); 
+    if(dest !== undefined && dest !== '') { // check existence
+        $('html').animate({ 
+            scrollTop: $(dest).offset().top - 80 // scroll to...
+        }, 500 // speed
+        );
+    }
+    return false;
+});
+
